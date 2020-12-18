ae4be69bf61b294b52e0a313f386fed0b312c674
Venkat.R <ramsunvtech@gmail.com>
2020-12-18 15:48:59 +0800

    feat(plp): Renamed product to product-list

---
+++ product-list/src/index.js
@@ -0,0 +1,3 @@
+document.querySelector('#plp-app').innerHTML = 'Product List (PLP) goes here';
+
+console.log('PLP App Loaded');
---
+++ product-list/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devServer: {
+    port: 8081,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "productList",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductListIndex": "./src/index",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, "static/index.html"),
+    }),
+  ],
+};
